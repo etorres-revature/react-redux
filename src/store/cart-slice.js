1e2e4fc43f1502a9f03ea87613353d0f9bcaee3a
Eric D. Torres <eric.d.torres@ibm.com>
2021-06-01 15:40:49 -0500

    Cart added to store

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,15 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const uiSlice = createSlice({
-  name: "ui",
-  initialState: { cartIsVisible: false },
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialState,
   reducers: {
-    toggle(state) {
-      state.cartIsVisible = !state.cartIsVisible;
+    addItemToCart(state, action) {
+      const addedItem = action.payload;
+
+      const existingItem = state.items.find((item) => item.id === addedItem.id);
+
+      const { id, price, title } = addedItem;
+
+      if (!existingItem) {
+        state.items.push({
+          itemID: id,
+          price,
+          quantity: 1,
+          totalPrice: price,
+          name: title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice = existingItem.price;
+      }
     },
   },
 });
 
-export const uiActions = uiSlice.actions;
+export const cartActions = cartSlice.actions;
 
-export default uiSlice;
+export default cartSlice;
