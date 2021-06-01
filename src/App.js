bb430c84e6bced097755b41afabe36f67945ff32
Eric D. Torres <eric.d.torres@ibm.com>
2021-06-01 16:48:07 -0500

    handling http status and dispatching notifications with useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending....",
+          message: "Sending Cart Data Now.",
+        })
+      );
+      const response = await fetch("urlGoesHere...", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success...",
+          message: "Cart Data sent successfully.",
+        })
+      );
+    };
+
+    sendCartData().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!!",
+          message: "Cart Data failed to send.",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
     <Layout>
