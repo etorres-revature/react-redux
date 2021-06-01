732046cc972770e961bf4112d5ab6c76555b2213
Eric D. Torres <eric.d.torres@ibm.com>
2021-06-01 16:53:51 -0500

    notification logic finished in app comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -40,6 +44,11 @@ function App() {
       );
     };
 
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
     sendCartData().catch((err) => {
       dispatch(
         uiActions.showNotification({
@@ -52,10 +61,17 @@ function App() {
   }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
+      />
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
